5e44d075e43af469a4185be588aa960705c2ec91
Alex Sharikhin <daclip@gmail.com>
2017-03-17 17:38:04 +0300

    Line numbers color

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -39,15 +39,19 @@
       desktop-save                t
       desktop-files-not-to-save   "^$" ;reload tramp paths
       desktop-load-locked-desktop nil)
-(desktop-save-mode 1)
+(desktop-save-mode 0)
 
 (defun my-desktop ()
   "Load the desktop and enable autosaving"
   (interactive)
   (let ((desktop-load-locked-desktop "ask"))
     (desktop-read)
-    (desktop-save-mode 1)))
+    (desktop-save-mode 0)))
     
+
+
+;;
+(set-face-foreground 'linum "#303030")
 
 ;;
 (provide 'init-local)
